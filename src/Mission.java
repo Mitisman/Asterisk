2f8df3e1f0d657e4e41d9a319b33a031b2096b60
Michou <34058527+Mitisman@users.noreply.github.com>
2018-06-03 22:07:14 +0200

    Update Mission.java

--- src/Mission.java
+++ src/Mission.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 import java.util.Random;
 
 public class Mission {
-	
+
 	public Mission (ArrayList<Joueur> j) {
 		this.joueurs = j;
 		
@@ -40,7 +40,7 @@ public class Mission {
 		}
 		DistributionM();
 	}
-	
+
 	public void DistributionM() {
 		Collections.shuffle(Mission);
 		Collections.shuffle(joueurs);
@@ -51,24 +51,200 @@ public class Mission {
 			if(Mission.get(alea) == mE) {
 				if(i<joueurs.size() - 1) {
 					Mission.remove(alea);
-					Mission.add(alea, "DÈtruire " + joueurs.get(i+1).getNom());
+					Mission.add(alea, "D√©truire " + joueurs.get(i+1).getNom());
 				} else {
 					Mission.remove(alea);
-					Mission.add(alea, "DÈtruire " + joueurs.get(i-1).getNom());
+					Mission.add(alea, "D√©truire " + joueurs.get(i-1).getNom());
 				}
 			}
 		j.setMission(Mission.get(alea));
 		}
 	}
 	
+	public boolean isDone(Joueur player) {
+		boolean principal = mCCheck(player);
+		boolean secondaire = false;
+		if(player.getMission().trim().equals(mA.trim())) {
+			secondaire = mACheck(player);
+		}else if(player.getMission().trim().equals(mB.trim())){
+			secondaire = mBCheck(player);
+		}else if(player.getMission().trim().equals(mD.trim())){
+			secondaire = mDCheck(player);
+		}else if(player.getMission().trim().equals(mE.trim())){
+			secondaire = mECheck(player);
+		}else if(player.getMission().trim().equals(mF.trim())){
+			secondaire = mFCheck(player);
+		}else if(player.getMission().trim().equals(mG.trim())){
+			secondaire = mGCheck(player);
+		}else if(player.getMission().trim().equals(mH.trim())){
+			secondaire = mHCheck(player);
+		}else {
+			System.out.println("COMMENT CA T'AS PAS DE MISSION?");
+		}
+		return principal && secondaire;
+	}
+	
+	public boolean mACheck(Joueur player) {
+		int check=0;
+		int AdN=0;
+		int AdS=0;
+		int Europe=0;
+		int Oceanie=0;
+		int Afrique=0;
+		ArrayList<Territoire> territoires = player.getTerritoire();
+		for(Territoire pays : territoires) {
+			if(pays.getRegion().trim().equals("Am√©riquedunord")) {
+				AdN++;
+			}else if(pays.getRegion().trim().equals("Am√©riquedusud")) {
+				AdS++;
+			}else if(pays.getRegion().trim().equals("Asie")) {
+				check++;
+			}else if(pays.getRegion().trim().equals("Europe")) {
+				Europe++;
+			}else if(pays.getRegion().trim().equals("Oc√©anie")) {
+				Oceanie++;
+			}else if(pays.getRegion().trim().equals("Afrique")){
+				Afrique++;
+			}else {
+				System.out.println("There might be a problem");
+			}
+		}
+		if ((check==12) && ( AdN==9 || AdS==4 || Europe==7 || Oceanie==4 || Afrique==6 )) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean mBCheck(Joueur player) {
+		int AdN=0;
+		int AdS=0;
+		int Asie=0;
+		int Europe=0;
+		int Oceanie=0;
+		int Afrique=0;
+		int count=0;
+		
+		ArrayList<Territoire> territoires = player.getTerritoire();
+		for(Territoire pays : territoires) {
+			if(pays.getRegion().trim().equals("Am√©riquedunord")) {
+				AdN++;
+			}else if(pays.getRegion().trim().equals("Am√©riquedusud")) {
+				AdS++;
+			}else if(pays.getRegion().trim().equals("Asie")) {
+				Asie++;
+			}else if(pays.getRegion().trim().equals("Europe")) {
+				Europe++;
+			}else if(pays.getRegion().trim().equals("Oc√©anie")) {
+				Oceanie++;
+			}else if(pays.getRegion().trim().equals("Afrique")){
+				Afrique++;
+			}else {
+				System.out.println("There might be a problem");
+			}
+		}
+		if(AdN==9) {
+			count++;
+		}
+		if(AdS==4) {
+			count++;
+		}
+		if(Asie==12) {
+			count++;
+		}
+		if(Europe==7) {
+			count++;
+		}
+		if(Oceanie==4) {
+			count++;
+		}
+		if(Afrique==6) {
+			count++;
+		}
+		
+		if(count>=3 && territoires.size()>=18) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean mCCheck(Joueur player) {
+		if(player.getTerritoire().size()==42) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean mDCheck(Joueur player) {
+		if(player.getTerritoire().size()>=30) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean mECheck(Joueur player) {
+		ArrayList<Joueur>players = new ArrayList<Joueur>();
+		players.addAll(joueurs);
+		Joueur joueurCible = player;
+		String [] mots = player.getMission().split(" ");
+		for(String mot : mots) {
+			for(int i=0;i<players.size();i++) {
+				if(mot.equals(players.get(i).getNom())) {
+					players.remove(i);
+				}
+			}
+		}
+		for(String word : mots) {
+			for(int j=0;j<players.size();j++) {
+				if(word.equals(players.get(j).getNom())) {
+					joueurCible = players.get(j);
+ 				}
+			}
+		}
+		if(joueurCible.getTerritoire().size()==0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean mFCheck(Joueur player) {
+		int check=0;
+		ArrayList<Territoire> territoires = player.getTerritoire();
+		for(Territoire pays : territoires) {
+			if(pays.getArmy().size()>=2) {
+				check++;
+			}
+		}
+		if (check>=18) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean mGCheck(Joueur player) {
+		if(player.getTerritoire().size()>=24) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean mHCheck(Joueur player) {
+		if(player.getTerritoire().size()>=21) {
+			return true;
+		}
+		return false;
+	}
+	
 	ArrayList<String> Mission = new ArrayList<>();
 	ArrayList<Joueur> joueurs = new ArrayList<>();
-	String mA = "ContrÙler la plus grosse rÈgion + 1 autre rÈgion";
-	String mB = "ContÙler 3 rÈgions et au moins 18 territoires";
-	String mC = "ConquÈrir tous les territoires";
-	String mD = "ContrÙler 30 territoires";
-	String mE = "DÈtruire X";
-	String mF = "ContrÙler 18 territoires avec au moins 2 armÈes";
-	String mG = "ContrÙler 24 territoires";
-	String mH = "contrÙler 21 territoires";
+	String mA = "Contr√¥ler la plus grosse r√©gion + 1 autre r√©gion";
+	String mB = "Cont√¥ler 3 r√©gions et au moins 18 territoires";
+	String mC = "Conqu√©rir tous les territoires";
+	String mD = "Contr√¥ler 30 territoires / Contr√¥ler la plan√®te";
+	String mE = "D√©truire X";
+	String mF = "Contr√¥ler 18 territoires avec au moins 2 arm√©es";
+	String mG = "Contr√¥ler 24 territoires";
+	String mH = "Contr√¥ler 21 territoires";
+	
+	
+	
 }
